211463a654e06dd7ac535fa5bd2274d77e67b95c
MiloBot2 <milo.vandam@student.rocva.nl>
2021-01-26 12:18:12 +0100

    linkje

--- 01-Challenge/Taak08-Eindgesprek/student-gp-link.md
+++ 01-Challenge/Taak08-Eindgesprek/student-gp-link.md
@@ -4,4 +4,4 @@ Student : `Milo`
 
 Klas    : `2127861`
 
-Link    : [github_username.github.io](MiloBot2.github.io)
+Link    : [github_username.github.io](https://sites.lumon.dev/milo/v2/portfolio/)
--- portfolio/includes/navbar.php
+++ portfolio/includes/navbar.php
@@ -1,12 +1,12 @@
 <header id="navBar" class="navBar">
     <h2 style="font-variant: small-caps; margin-right: auto; color: #49ba94;">Milo's Website</h2>
-    <link href="http://localhost/milo/css/nav.css" rel="stylesheet">
+    <link href="/portfolio/css/nav.css" rel="stylesheet">
     <nav>
         <ul class="nav_links"> 
-            <li id="home"><a href='http://localhost/milo/'>About</a></li>
-            <li><a href='http://localhost/milo/interests'>Interests</a></li>
-            <li><a href='http://localhost/milo/projects'>Projects</a></li>
-            <li><a href='http://localhost/milo/contact'>Contact</a></li>
+            <li id="home"><a href='/portfolio/'>About</a></li>
+            <li><a href='/portfolio/interests'>Interests</a></li>
+            <li><a href='/portfolio/projects'>Projects</a></li>
+            <li><a href='/portfolio/contact'>Contact</a></li>
         </ul>
     </nav>
     <a href="javascript:void(0);" class="icon" onclick="resNav()">
